a7e4fd568ac70cb0cbfd1458bde46c48ecb54cc8
outli2r <80079529+outli2r@users.noreply.github.com>
2022-10-21 10:01:12 +0900

    uploaded at 2022. 10. 21. 오전 10:01:11

---
+++ baekjoon/1068/solution.py
@@ -0,0 +1,48 @@
+// [문제 링크]: https://www.acmicpc.net/problem/1068
+
+import sys
+# sys.stdin = open("./test.txt", "r")
+input = sys.stdin.readline
+from collections import defaultdict
+​
+​
+def solution():
+    def dfs(node):
+        checked[node] = 1
+​
+        if node in tree and not tree[node]:
+            del tree[node]
+            return
+​
+        for n in tree[node]:
+            if not checked[n]:
+                dfs(n)
+​
+        del tree[node]
+​
+        return
+​
+    checked = defaultdict(int)
+    dfs(target)
+​
+    leaf = 0
+    for t in tree:
+        if not tree[t]:
+            leaf += 1
+    if l[target] != -1 and len(tree[l[target]]) == 1:
+        leaf += 1
+    print(leaf)
+    return
+​
+​
+N = int(input())
+l = list(map(int, input().split()))
+tree = {i: [] for i in range(N)}
+target = int(input())
+for i in range(N):
+    if l[i] == -1:
+        continue
+    tree[l[i]].append(i)
+​
+solution()
+​
