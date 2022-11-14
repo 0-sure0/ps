bfd27115ea332235f01f0fc95a1939aab8b32770
outli2r <80079529+outli2r@users.noreply.github.com>
2022-11-14 09:22:07 +0900

    updated at 2022. 11. 14. 오전 9:22:07

--- baekjoon/5639/solution.py
+++ baekjoon/5639/solution.py
@@ -1,31 +1,40 @@
 // [문제 링크]: https://www.acmicpc.net/problem/5639
 
 import sys
-sys.setrecursionlimit(100000)
+#sys.stdin = open('test.txt', 'r')
 input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
 ​
+def postorder(tree):
+    if not tree:
+        return
 ​
-def getPostorder(nums):
-    length = len(nums)
+    if len(tree) == 1:
+        print(tree[0])
+        return
 ​
-    if length <= 1:
-        return nums
-​
-    for i in range(1, length):
-        if nums[i] > nums[0]:
-            return getPostorder(nums[1:i]) + getPostorder(nums[i:]) + [nums[0]]
-​
-    return getPostorder(nums[1:]) + [nums[0]]
-​
-​
-if __name__ == '__main__':
-    nums = []
-    while True:
-        try:
-            nums.append(int(input()))
-        except:
+    root = tree[0]
+    mid = len(tree)
+    for i in range(1, len(tree)):
+        if root < tree[i]:
+            mid = i
             break
 ​
-    nums = getPostorder(nums)
-    for n in nums:
-        print(n)
+    if mid == len(tree):
+        postorder(tree[1:])
+    else:
+        postorder(tree[1:mid])
+        postorder(tree[mid:])
+    print(root)
+​
+    return
+​
+l = []
+while True:
+    try:
+        n = int(input())
+        l.append(n)
+    except ValueError:
+        break
+​
+postorder(l)
