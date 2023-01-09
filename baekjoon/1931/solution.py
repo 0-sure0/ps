4603cced453d2e1c95be2a3164677e451bbef542
outli2r <80079529+outli2r@users.noreply.github.com>
2023-01-09 14:49:02 +0900

    updated at 2023. 1. 9. 오후 2:49:01

--- baekjoon/1931/solution.py
+++ baekjoon/1931/solution.py
@@ -6,20 +6,18 @@ input = sys.stdin.readline
 ​
 def solution():
     N = int(input())
-    conference = sorted([list(map(int, input().split())) for _ in range(N)], key=lambda x: (x[1], x[0]))
+    conf = sorted([list(map(int, input().split())) for _ in range(N)], key=lambda x: (x[1], x[0]))
+    end = conf[0][1]
     ans = 1
-    end = conference[0][1]
 ​
-    for i in range(1, len(conference)):
-        s, e = conference[i]
+    for i in range(1, len(conf)):
+        s, e = conf[i]
         if end <= s:
             end = e
             ans += 1
-            
+​
     print(ans)
-​
     return
-​
 ​
 solution()
 ​
