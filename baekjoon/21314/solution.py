72ea8b68709a9f63bd093b94f57c92b6495d61e2
outli2r <80079529+outli2r@users.noreply.github.com>
2023-01-09 13:51:41 +0900

    updated at 2023. 1. 9. 오후 1:51:40

--- baekjoon/21314/solution.py
+++ baekjoon/21314/solution.py
@@ -5,32 +5,27 @@ import sys
 input = sys.stdin.readline
 ​
 def solution():
+    min_ans = max_ans = ""
     s = input().rstrip()
-    max_s = min_s = ''
-​
     cnt = 0
     for c in s:
         if c == 'M':
             cnt += 1
         else:
             if cnt > 0:
-                max_s += str(5 * (10 ** cnt))
-                min_s += str(10 ** (cnt - 1)) + '5'
-                cnt = 0
+                max_ans += str(5 * 10 ** cnt)
+                min_ans += str(10 ** (cnt - 1)) + '5'
             else:
-                max_s += '5'
-                min_s += '5'
+                max_ans += '5'
+                min_ans += '5'
+            cnt = 0
 ​
-    if cnt > 0:
-        max_s += '1' * cnt
-        min_s += str(10 ** (cnt - 1))
-​
-    print(max_s)
-    print(min_s)
-​
-​
+    if cnt != 0:
+        min_ans += str(10 ** (cnt - 1))
+        max_ans += '1' * cnt
+    print(max_ans)
+    print(min_ans)
     return
-​
 ​
 solution()
 ​
