164b4d504618eb2604a8c449e796b7cab0e915ab
outli2r <80079529+outli2r@users.noreply.github.com>
2023-02-01 16:08:52 +0900

    uploaded at 2023. 2. 1. 오후 4:08:52

---
+++ baekjoon/2141/solution.py
@@ -0,0 +1,24 @@
+// [문제 링크]: https://www.acmicpc.net/problem/2141
+
+import sys
+input = sys.stdin.readline
+​
+X = []
+people = 0
+n = int(input())
+for i in range(n):
+    village, num = map(int,input().split())
+    X.append([village, num])
+    people+=num
+​
+X.sort(key = lambda x : x[0])
+​
+count = 0
+po = 0
+for i in range(n):
+    count+=X[i][1]
+    if count >= people/2:
+      po = X[i][0]
+      break 
+​
+print(po)
