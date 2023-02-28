f9aeba3962d4f81ab50db1152854fcd48b23fb8b
outli2r <80079529+outli2r@users.noreply.github.com>
2023-02-28 10:22:26 +0900

    updated at 2023. 2. 28. 오전 10:22:25

--- baekjoon/1890/solution.py
+++ baekjoon/1890/solution.py
@@ -3,9 +3,11 @@
 import sys
 #sys.stdin = open('test.txt', 'r')
 input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
 ​
 def solution():
     N = int(input())
+    dir = [(0, 1), (1, 0)]
     board = [list(map(int, input().split())) for _ in range(N)]
     dp = [[0] * N for _ in range(N)]
     dp[0][0] = 1
@@ -13,15 +15,13 @@ def solution():
     for r in range(N):
         for c in range(N):
             if r == N - 1 and c == N - 1:
-                print(dp[r][c])
-                continue
-            move = board[r][c]
-            if c + move < N:
-                dp[r][c + move] += dp[r][c]
-​
-            if r + move < N:
-                dp[r + move][c] += dp[r][c]
-​
+                break
+            for i in range(2):
+                nr = r + dir[i][0] * board[r][c]
+                nc = c + dir[i][1] * board[r][c]
+                if 0 <= nr < N and 0 <= nc < N:
+                    dp[nr][nc] += dp[r][c]
+    print(dp[N-1][N-1])
     return
 ​
 solution()
