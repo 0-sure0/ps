3f7368ea075e5493ae68d6bc7b39771960035146
outli2r <80079529+outli2r@users.noreply.github.com>
2023-03-03 14:53:07 +0900

    updated at 2023. 3. 3. 오후 2:53:06

--- baekjoon/1106/solution.py
+++ baekjoon/1106/solution.py
@@ -3,19 +3,22 @@
 import sys
 #sys.stdin = open('test.txt', 'r')
 input = sys.stdin.readline
+from math import inf
 ​
 def solution():
     C, N = map(int, input().split())
     l = [list(map(int, input().split())) for _ in range(N)]
-    dp = [sys.maxsize] * (C + 100)
-    dp[0] = 0
+    l.sort(key=lambda x: x[0], reverse=True)
+    dp = [0] + [inf] * 1100
 ​
-    for c, p in l:
-        for i in range(p, C + 100):
-            dp[i] = min(dp[i-p] + c, dp[i])
+    for cost, people in l:
+        for i in range(C + 1):
+            dp[i + people] = min(dp[i + people], dp[i] + cost)
+​
 ​
     print(min(dp[C:]))
     return
+​
 ​
 solution()
 ​
