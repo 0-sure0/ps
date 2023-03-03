c41d0ae8df40f94b6be8470223f772a096c0ff88
outli2r <80079529+outli2r@users.noreply.github.com>
2023-03-03 09:59:20 +0900

    updated at 2023. 3. 3. 오전 9:59:21

--- baekjoon/22869/solution.py
+++ baekjoon/22869/solution.py
@@ -4,26 +4,26 @@ import sys
 #sys.stdin = open('test.txt', 'r')
 input = sys.stdin.readline
 ​
+​
 def solution():
     N, K = map(int, input().split())
     stone = list(map(int, input().split()))
-    dp = [0] * N
-    dp[0] = 1
-    j = 1
+    dp = ['YES' if i == 0 else 'NO' for i in range(N)]
 ​
-    while j < N:
-        if dp[j] == 1:
-            continue
+    def power(i, j):
+        return (i - j) * (1 + abs(stone[i] - stone[j]))
 ​
-        for i in range(j-1, -1, -1):
-            if dp[i] and (j - i) * (1 + abs(stone[i] - stone[j])) <= K:
-                dp[j] = 1
+    for i in range(1, N):
+        for j in range(0, i):
+            if dp[j] == 'NO':
+                continue
+            if power(i, j) <= K:
+                dp[i] = 'YES'
                 break
-        j += 1
 ​
-    ans = 'NO' if dp[-1] != 1 else 'YES'
-    print(ans)
+    print(dp[-1])
     return
+​
 ​
 solution()
 ​
