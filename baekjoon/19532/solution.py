fc9cc25a6431e1140ccbf1453205475776944fc5
outli2r <80079529+outli2r@users.noreply.github.com>
2023-05-16 11:25:23 +0900

    updated at 2023. 5. 16. 오전 11:25:22

--- baekjoon/19532/solution.py
+++ baekjoon/19532/solution.py
@@ -3,17 +3,14 @@
 import sys
 #sys.stdin = open('test.txt', 'r')
 input = sys.stdin.readline
-from itertools import product
 ​
 def solution():
     a, b, c, d, e, f = map(int, input().split())
-    for x, y in product(range(-999, 1000), repeat=2):
-        if a * x + b * y == c and d * x + e * y == f:
-            print(x, y)
-            return
-​
-​
-    return
+    for x in range(-999, 1000):
+        for y in range(-999, 1000):
+            if a * x + b * y == c and d * x + e * y == f:
+                print(x, y)
+                return
 ​
 ​
 solution()
