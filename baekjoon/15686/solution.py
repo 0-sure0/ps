066fe6bbfd50b68a48d614a0395cf4035a8f4291
outli2r <80079529+outli2r@users.noreply.github.com>
2023-05-30 10:21:56 +0900

    updated at 2023. 5. 30. 오전 10:21:55

--- baekjoon/15686/solution.py
+++ baekjoon/15686/solution.py
@@ -1,60 +1,35 @@
 // [문제 링크]: https://www.acmicpc.net/problem/15686
 
 import sys
-# sys.stdin = open("./test.txt", "r")
+#sys.stdin = open('test.txt', 'r')
 input = sys.stdin.readline
-​
-def dist(h):
-    l = []
-    for i in range(len(target)):
-        min_d = 1000000
-        for j in range(len(target[i])):
-            min_d = min(min_d, abs(h[0] - target[i][j][0]) + abs(h[1] - target[i][j][1]))
-        l.append(min_d)
-​
-    return l
-​
-​
-def comb(l, depth):
-    if len(l) == m:
-        target.append(l)
-        return
-​
-    elif depth == len(chicken):
-        return
-​
-    l.append(chicken[depth])
-    comb(l[:], depth + 1)
-​
-    l.pop()
-    comb(l[:], depth + 1)
-​
-    return
+from itertools import combinations
 ​
 ​
 def solution():
-    comb([], 0)
-    chicken_d = []
-    for h in home:
-        chicken_d.append(dist(h))
+    n, m = map(int, input().split())
+    chicken = []
+    house = []
+    answer = sys.maxsize
+    for r in range(n):
+        t = list(map(int, input().split()))
+        for c in range(n):
+            if t[c] == 2:
+                chicken.append((r, c))
+            elif t[c] == 1:
+                house.append((r, c))
 ​
-    ans = list(map(sum, zip(*chicken_d)))
-    return min(ans)
+    for comb in combinations(chicken, m):
+        tmp = 0
+        for r1, c1 in house:
+            d = sys.maxsize
+            for r2, c2 in comb:
+                d = min(d, abs(r1 - r2) + abs(c1 - c2))
+            tmp += d
+        answer = min(answer, tmp)
+​
+    print(answer)
+    return
 ​
 ​
-n, m = map(int, input().split())
-chicken = []
-board = []
-target = []
-home = []
-​
-for i in range(n):
-    t = list(map(int, input().split()))
-    for j in range(n):
-        if t[j] == 2:
-            chicken.append([i, j])
-        elif t[j] == 1:
-            home.append([i, j])
-    board.append(t)
-​
-print(solution())
+solution()
