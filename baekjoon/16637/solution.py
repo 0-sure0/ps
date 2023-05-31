ddb8ff604f6ff588fb30bfa36ce7e7718e481e15
outli2r <80079529+outli2r@users.noreply.github.com>
2023-05-31 20:24:00 +0900

    updated at 2023. 5. 31. 오후 8:23:59

--- baekjoon/16637/solution.py
+++ baekjoon/16637/solution.py
@@ -1,31 +1,41 @@
 // [문제 링크]: https://www.acmicpc.net/problem/16637
 
 import sys
+#sys.stdin = open('test.txt', 'r')
+input = sys.stdin.readline
+import math
 ​
-n = int(input())
-s = input()
-result = -1 * sys.maxsize
+def solution():
+    n: int = int(input())
+    s: str = input().rstrip()
+    result: int = -math.inf
 ​
-def myOperator(num1, oper, num2):
-    if oper == '+':
-        return num1 + num2
-    if oper == '-':
-        return num1 - num2
-    if oper == '*':
-        return num1 * num2
+    def calc(n1: int, op: int, n2: int):
+        if op == '+':
+            return n1 + n2
+        elif op == '-':
+            return n1 - n2
+        elif op == '*':
+            return n1 * n2
 ​
-def dfs(index, value):
-    global result
+    def dfs(idx: int, value: int):
+        nonlocal result
+        if idx == n - 1:
+            result = max(result, value)
+            return
 ​
-    if index == n - 1:
-        result = max(result, value);
+        if idx + 2 < n:
+            dfs(idx + 2, calc(value, s[idx + 1], int(s[idx + 2])))
+​
+        if idx + 4 < n:
+            dfs(idx + 4, calc(value, s[idx + 1], calc(int(s[idx + 2]), s[idx + 3], int(s[idx + 4]))))
+​
         return
 ​
-    if index + 2 < n:
-        dfs(index + 2, myOperator(value, s[index + 1], int(s[index + 2])))
+    dfs(0, int(s[0]))
+    print(result)
+    return
 ​
-    if index + 4 < n:
-        dfs(index + 4, myOperator(value, s[index + 1], myOperator(int(s[index + 2]), s[index + 3], int(s[index + 4]))))
+solution()
 ​
-dfs(0, int(s[0]))
-print(result)
+​
