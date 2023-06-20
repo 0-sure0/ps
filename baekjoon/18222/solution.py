5ff48a413d09009be3b9bdd1a31cda1a9da5b9c7
outli2r <80079529+outli2r@users.noreply.github.com>
2023-06-20 11:17:59 +0900

    uploaded at 2023. 6. 20. 오전 11:17:59

---
+++ baekjoon/18222/solution.py
@@ -0,0 +1,24 @@
+// [문제 링크]: https://www.acmicpc.net/problem/18222
+
+import sys
+#sys.stdin = open('test.txt', 'r')
+input = sys.stdin.readline
+​
+​
+def solution():
+    k = int(input())
+    cnt = 0
+​
+    while k:
+        cur = 1
+        while cur * 2 < k:
+            cur <<= 1
+        k -= cur
+        cnt += 1
+​
+    ans = ~cnt & 1
+    print(ans)
+    return
+​
+solution()
+​
