19228a393f7e2e6c800f46a9e6875b3aa7fd51fc
outli2r <80079529+outli2r@users.noreply.github.com>
2023-06-26 10:02:21 +0900

    uploaded at 2023. 6. 26. 오전 10:02:20

---
+++ baekjoon/18352/solution.py
@@ -0,0 +1,41 @@
+// [문제 링크]: https://www.acmicpc.net/problem/18352
+
+import sys
+#sys.stdin = open('test.txt', 'r')
+input = sys.stdin.readline
+from collections import defaultdict, deque
+​
+​
+def solution():
+    n, m, k, x = map(int, input().split())
+    graph = defaultdict(list)
+    ans = defaultdict(list)
+    for _ in range(m):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+​
+    def bfs(start):
+        checked = [0] * (n + 1)
+        q = deque([start])
+        depth = 0
+        while q:
+            for _ in range(len(q)):
+                node = q.popleft()
+                checked[node] = 1
+                ans[depth].append(node)
+                for next_node in graph[node]:
+                    if not checked[next_node]:
+                        checked[next_node] = 1
+                        q.append(next_node)
+            depth += 1
+        return
+​
+    bfs(x)
+    if not ans[k]:
+        print(-1)
+    else:
+        print(*sorted(ans[k]), sep='\n')
+    return
+​
+solution()
+​
