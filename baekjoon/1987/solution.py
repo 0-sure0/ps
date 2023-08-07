1c7d640b03d1f0eb80bc350655c5df483418f1af
outli2r <80079529+outli2r@users.noreply.github.com>
2023-08-07 12:19:30 +0900

    updated at 2023. 8. 7. 오후 12:19:29

--- baekjoon/1987/solution.py
+++ baekjoon/1987/solution.py
@@ -1,21 +1,29 @@
 // [문제 링크]: https://www.acmicpc.net/problem/1987
 
-n, m = map(int, input().split())
-a = [input() for _ in range(n)]
-dirs = ((1, 0), (0, 1), (-1, 0), (0, -1))
-ans, used = 0, [False for _ in range(26)]  # 정답 변수 & 사용한 알파벳 기록 배열
-def backtracking(x: int, y: int, cnt: int):  # 현재 위치 (x, y) 와 이동 거리 (cnt)를 가지고 남은 모든 경우를 탐색해주는 함수
-    global ans
-    ans = max(ans, cnt)
-    used[ord(a[x][y]) - ord('A')] = True
-    
-    for dx, dy in dirs:
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < n and 0 <= ny < m and not used[ord(a[nx][ny]) - ord('A')]:
-            backtracking(nx, ny, cnt + 1)
-    
-    used[ord(a[x][y]) - ord('A')] = False
-    
-backtracking(0, 0, 1)
-print(ans)
+import sys
+#sys.stdin = open('test.txt', 'r')
+input = sys.stdin.readline
+​
+​
+def solution():
+    R, C = map(int, input().split())
+    board = [list(input().rstrip()) for _ in range(R)]
+    q = set([(0, 0, board[0][0])])
+    answer = 0
+    dir = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+​
+    while q:
+        for _ in range(len(q)):
+            r, c, chars = q.pop()
+            answer = max(answer, len(chars))
+            for i in range(4):
+                nr = r + dir[i][0]
+                nc = c + dir[i][1]
+                if 0 <= nr < R and 0 <= nc < C and board[nr][nc] not in chars:
+                    q.add((nr, nc, chars + board[nr][nc]))
+​
+    print(answer)
+    return
+​
+solution()
 ​
