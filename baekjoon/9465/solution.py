7d5afc6a8fa4d67ca0bb9bafa758fcc4f1cfe733
outli2r <80079529+outli2r@users.noreply.github.com>
2023-08-08 12:57:34 +0900

    updated at 2023. 8. 8. 오후 12:57:35

--- baekjoon/9465/solution.py
+++ baekjoon/9465/solution.py
@@ -1,16 +1,26 @@
 // [문제 링크]: https://www.acmicpc.net/problem/9465
 
-t = int(input())
-for i in range(t):
-  s = []
-  n = int(input())
-  for k in range(2):
-    s.append(list(map(int, input().split())))
-  for j in range(1, n):
-    if j == 1:
-      s[0][j] += s[1][j - 1]
-      s[1][j] += s[0][j - 1]
-    else:
-      s[0][j] += max(s[1][j - 1], s[1][j - 2])
-      s[1][j] += max(s[0][j - 1], s[0][j - 2])
-  print(max(s[0][n - 1], s[1][n - 1]))
+import sys
+#sys.stdin = open('test.txt', 'r')
+input = sys.stdin.readline
+​
+​
+def solution():
+    T = int(input())
+    for _ in range(T):
+        n = int(input())
+        l = [list(map(int, input().split())) for _ in range(2)]
+        dp = [[0] * n for _ in range(2)]
+        dp[0][0] = l[0][0]
+        dp[1][0] = l[1][0]
+​
+        for c in range(1, n):
+            for r in range(2):
+                dp[r][c] = max(l[r][c] + dp[(r + 1) % 2][c - 1], dp[r][c - 1])
+​
+        print(max(dp[0][-1], dp[1][-1]))
+​
+    return
+​
+solution()
+​
