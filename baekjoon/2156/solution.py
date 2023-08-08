45223e4222f42f9856f60ac381bd860a245f3a81
outli2r <80079529+outli2r@users.noreply.github.com>
2023-08-08 17:19:28 +0900

    updated at 2023. 8. 8. 오후 5:19:29

--- baekjoon/2156/solution.py
+++ baekjoon/2156/solution.py
@@ -4,22 +4,24 @@ import sys
 #sys.stdin = open('test.txt', 'r')
 input = sys.stdin.readline
 ​
+​
 def solution():
     n = int(input())
     wine = [int(input()) for _ in range(n)]
-    if n == 1:
-        print(wine[0])
+​
+    if n <= 2:
+        print(sum(wine))
         return
-    elif n == 2:
-        print(wine[0] + wine[1])
-        return
-    dp = [wine[0], wine[0] + wine[1], max(wine[0] + wine[2], wine[1] + wine[2], wine[0] + wine[1])]
+​
+    dp = [0] * n
+    dp[0] = wine[0]
+    dp[1] = wine[0] + wine[1]
+    dp[2] = max(wine[0] + wine[2], wine[1] + wine[2], dp[1])
 ​
     for i in range(3, n):
-        dp.append(max(dp[i-1], dp[i-3] + wine[i-1] + wine[i], wine[i] + dp[i-2]))
+        dp[i] = max(wine[i] + wine[i - 1] + dp[i - 3], wine[i] + dp[i - 2], dp[i - 1])
 ​
     print(dp[-1])
-​
     return
 ​
 solution()
