558d7e60e514f898db17053b1ac9089522da9eb5
outli2r <80079529+outli2r@users.noreply.github.com>
2023-08-20 09:41:25 +0900

    updated at 2023. 8. 20. 오전 9:41:26

--- baekjoon/15486/solution.py
+++ baekjoon/15486/solution.py
@@ -1,19 +1,22 @@
 // [문제 링크]: https://www.acmicpc.net/problem/15486
 
-import sys 
-input = sys.stdin.readline 
+import sys
+#sys.stdin = open('test.txt', 'r')
+input = sys.stdin.readline
 ​
-n = int(input())
-t,p = [],[]
-dp = [0] * (n+1)
-for i in range(n):
-    x,y = map(int,input().split())
-    t.append(x)
-    p.append(y)
-M = 0 
-for i in range(n):
-    M = max(M,dp[i])  
-    if i+t[i] > n :  
-        continue 
-    dp[i+t[i]] = max(M+p[i],dp[i+t[i]])
-print(max(dp))
+​
+def solution():
+    n = int(input())
+    work = [list(map(int, input().split())) for _ in range(n)]
+    dp = [0] * (n + 1)
+    cur_max = 0
+    for i, (t, p) in enumerate(work):
+        cur_max = max(cur_max, dp[i])
+        if i + t <= n:
+            dp[i + t] = max(dp[i + t], cur_max + p)
+​
+    print(max(dp))
+    return
+​
+solution()
+​
