f981ccb76c1c1ce0838573975c739d0b344395ec
outli2r <80079529+outli2r@users.noreply.github.com>
2023-09-12 11:23:48 +0900

    updated at 2023. 9. 12. 오전 11:23:47

--- baekjoon/17836/solution.py
+++ baekjoon/17836/solution.py
@@ -1,45 +1,40 @@
 // [문제 링크]: https://www.acmicpc.net/problem/17836
 
 import sys
-#ys.stdin = open('test.txt', 'r')
+#sys.stdin = open('test.txt', 'r')
 input = sys.stdin.readline
 from collections import deque
 ​
+​
 def solution():
-    N, M, T = map(int, input().split())
-    board = [list(map(int, input().split())) for _ in range(N)]
+    n, m, T = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(n)]
+    dir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    answer = 10001
+    time = 0
 ​
-    def bfs():
-        nonlocal ans
+    q = deque([[0, 0]])
+    visited = [[0] * m for _ in range(n)]
+    while time < T:
+        time += 1
+        for _ in range(len(q)):
+            r, c = q.popleft()
+            for d in range(4):
+                nr = r + dir[d][0]
+                nc = c + dir[d][1]
 ​
-        dir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        checked = [[0] * M for _ in range(N)]
-        q = deque([(0, 0)])
-        cnt = 0
-        checked[0][0] = 1
-        while q and cnt < T:
-            cnt += 1
-            for _ in range(len(q)):
-                r, c = q.popleft()
-                for d in range(4):
-                    nr = r + dir[d][0]
-                    nc = c + dir[d][1]
-                    if 0 <= nr < N and 0 <= nc < M and not checked[nr][nc] and board[nr][nc] != 1:
-                        if (nr, nc) == (N - 1, M - 1):
-                            ans = min(ans, cnt)
-                            continue
-                        if board[nr][nc] == 0:
-                            q.append((nr, nc))
-                            checked[nr][nc] = 1
-                        elif board[nr][nc] == 2:
-                            ans = min(ans, cnt + (N - 1 - nr) + (M - 1 - nc))
-        return
+                if 0 <= nr < n and 0 <= nc < m and not visited[nr][nc] and board[nr][nc] != 1:
+                    if (nr, nc) == (n - 1, m - 1):
+                        answer = min(answer, time)
+                        continue
+                    if board[nr][nc] == 2:
+                        if time + (n - 1 - nr) + (m - 1 - nc) <= T:
+                            answer = min(answer, time + (n - 1 - nr) + (m - 1 - nc))
+                    else:
+                        q.append((nr, nc))
+                        visited[nr][nc] = 1
 ​
-    ans = sys.maxsize
-    bfs()
-    print("Fail" if ans == sys.maxsize or ans > T else ans)
+    print(answer if answer != 10001 else "Fail")
     return
 ​
-​
 solution()
-​
