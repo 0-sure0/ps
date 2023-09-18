726ff0a050f87214f39873c29dc8172bdb500aca
outli2r <80079529+outli2r@users.noreply.github.com>
2023-09-18 09:33:07 +0900

    updated at 2023. 9. 18. 오전 9:33:07

--- baekjoon/22946/solution.py
+++ baekjoon/22946/solution.py
@@ -1,54 +1,51 @@
 // [문제 링크]: https://www.acmicpc.net/problem/22946
 
-from collections import defaultdict
 import sys
 #sys.stdin = open('test.txt', 'r')
 input = sys.stdin.readline
+from collections import defaultdict
 import math
+sys.setrecursionlimit(10 ** 9)
 ​
-n = int(input())
-circles = [list(map(int, input().split())) for _ in range(n)]
-circles.append((0, 0, 2000000))
-inside = [(i, []) for i in range(n+1)]
+def solution():
+    N = int(input())
+    circles = [(i, list(map(int, input().split()))) for i in range(1, N + 1)]
+    circles.sort(key=lambda x: x[1][2])
+    graph = defaultdict(list)
+    p = [0] * (N + 1)
+    end_node = []
+    for i in range(N):
+        ai, (ax, ay, ar) = circles[i]
+        for j in range(i + 1, N):
+            bi, (bx, by, br) = circles[j]
+            d = math.sqrt((ax - bx) ** 2 + (ay - by) ** 2)
+            if d < abs(br - ar):
+                graph[ai].append(bi)
+                graph[bi].append(ai)
+                p[ai] = bi
+                break
+        if not p[ai]:
+            graph[ai].append(0)
+            graph[0].append(ai)
+        if len(graph[ai]) == 1:
+            end_node.append(ai)
 ​
-for i in range(n+1):
-    for j in range(n+1):
-        if i == j:
-            continue
-        Xa, Ya, Ra = circles[i]
-        Xb, Yb, Rb = circles[j]
-        distance = math.sqrt((Xa-Xb) * (Xa-Xb) + (Ya-Yb) * (Ya-Yb))
-        Rab = Ra-Rb
-        if distance < Rab:
-            inside[i][1].append(j)
+    def dfs(node, depth):
+        nonlocal answer
 ​
-inside.sort(key=lambda x: len(x[1]))
+        visited[node] = 1
+        for n in graph[node]:
+            if not visited[n]:
+                dfs(n, depth + 1)
+        answer = max(answer, depth)
+        return
 ​
-res = defaultdict(int)
+    answer = 0
+    for s in end_node:
+        visited = [0] * (N + 1)
+        dfs(s, 0)
 ​
-route = [[] for _ in range(n + 1)]
-visit = [True for _ in range(n + 1)]
-res = defaultdict(int)
+    print(answer)
+    return
 ​
-for v, children in inside:
-    for child in children:
-        if visit[child]:
-            route[v].append(child)
-            visit[child] = False
-        res[v] = max(res[v], res[child] + 1)
-​
-result = 0
-​
-for i in range(n+1):
-    result = max(result, res[i])
-    for child1 in route[i]:
-        for child2 in route[i]:
-            if child1 == child2:
-                continue
-            a = res[child1]
-            b = res[child2]
-            result = max(result, a+b+2)
-​
-​
-print(result)
-​
+solution()
