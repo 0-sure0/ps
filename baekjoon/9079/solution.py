daa13426569bd32cf3763d624c95e594561a28bd
outli2r <80079529+outli2r@users.noreply.github.com>
2023-09-21 10:13:17 +0900

    updated at 2023. 9. 21. 오전 10:13:17

--- baekjoon/9079/solution.py
+++ baekjoon/9079/solution.py
@@ -9,33 +9,45 @@ from collections import deque
 def solution():
     T = int(input())
 ​
-    def flip(numbers, arr):
-        for number in numbers:
-            arr[number] = '1' if arr[number] == '0' else '0'
-        return arr
-​
-    def bfs():
-        cases = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
-        visited = [0] * 512
-        visited[int(''.join(arr), 2)] = 1
-        q = deque([(int(''.join(arr), 2), 0)])
+    def bfs(num):
+        q = deque()
+        q.append((num, 0))
+        int_num = int(''.join(num), 2)
+        visited[int_num] = 1
 ​
         while q:
-            arrbin, cnt = q.popleft()
-            if arrbin == 0 or arrbin == 511:
+            num, cnt = q.popleft()
+            int_num = int(''.join(num), 2)
+            if int_num == 0 or int_num == 511:
                 return cnt
 ​
-            for i, numbers in enumerate(cases):
-                newarr = int(''.join(flip(numbers, list(bin(arrbin)[2:].zfill(9)))), 2)
-                if not visited[newarr]:
-                    visited[newarr] = 1
-                    q.append((newarr, cnt + 1))
+            for case in cases:
+                t = num[:]
+                for c in case:
+                    if num[c] == '1':
+                        t[c] = '0'
+                    else:
+                        t[c] = '1'
+                int_t = int(''.join(t), 2)
+                if not visited[int_t]:
+                    visited[int_t] = 1
+                    q.append((t, cnt + 1))
+​
         return -1
 ​
     for _ in range(T):
-        arr = list(list(input().split()) for _ in range(3))
-        arr = ['1' if arr[y][x] == 'H' else '0' for y in range(3) for x in range(3)]
-        print(bfs())
+        num = []
+        visited = [0] * 512
+        cases = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
+        for _ in range(3):
+            s = input().split()
+            for c in s:
+                if c == 'H':
+                    num.append('1')
+                else:
+                    num.append('0')
+​
+        print(bfs(num))
 ​
     return
 ​
