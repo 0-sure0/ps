7b6fe7d027e0584e797c266c678b120081242114
outli2r <80079529+outli2r@users.noreply.github.com>
2023-09-25 14:08:03 +0900

    updated at 2023. 9. 25. 오후 2:08:02

--- baekjoon/2470/solution.py
+++ baekjoon/2470/solution.py
@@ -1,43 +1,33 @@
 // [문제 링크]: https://www.acmicpc.net/problem/2470
 
-from bisect import bisect_left
 import sys
-def input():
-    return sys.stdin.readline().rstrip()
+#sys.stdin = open('test.txt', 'r')
+input = sys.stdin.readline
 ​
-n = int(input())
-lst_pos, lst_neg = [], []
-input_lst = list(map(int, input().split()))
-for x in input_lst:
-    if x > 0: lst_pos.append(x)
-    else: lst_neg.append(x)
 ​
-lst_pos.sort()
-lst_neg.sort()
+def solution():
+    N = int(input())
+    ls = sorted(map(int, input().split()))
+    answer = []
+    tmp = sys.maxsize
+    l, r = 0, N - 1
 ​
-if not lst_pos: print(*lst_neg[-2:]) # 양수가 없는 경우
-elif not lst_neg: print(*lst_pos[:2]) # 음수가 없는 경우
-else:
-    tmp = 2000000001 #최악 케이스+1
-    if len(lst_neg) > 1: # 음수만 가지고 답일 수 있음
-        if tmp > abs(sum(lst_neg[-2:])):
-            tmp = abs(sum(lst_neg[-2:]))
-            sol = lst_neg[-2:]
+    while l < r:
+        if abs(ls[l] + ls[r]) < tmp:
+            answer = [ls[l], ls[r]]
+            tmp = abs(ls[l] + ls[r])
 ​
-    if len(lst_pos) > 1: # 양수만 가지고 답일 수 있음
-        if tmp > abs(sum(lst_pos[:2])):
-            tmp = abs(sum(lst_pos[:2]))
-            sol = lst_pos[:2]  
+        if ls[l] + ls[r] == 0:
+            print(ls[l], ls[r])
+            return
 ​
-    for num in lst_neg:
-        idx = bisect_left(lst_pos, -num)
-        if idx == len(lst_pos): idx -= 1 # 마지막 인덱스
-        elif idx == 0: pass # 처음 인덱스
-        elif abs(num+lst_pos[idx]) > abs(num+lst_pos[idx-1]): # 둘 중 최적 선택
-            idx -= 1
+        elif ls[l] + ls[r] > 0:
+            r -= 1
+        else:
+            l += 1
 ​
-        if tmp > abs(num+lst_pos[idx]): # 필요 시 업데이트
-            tmp = abs(num+lst_pos[idx])
-            sol = sorted([num, lst_pos[idx]])
+    print(*answer)
+    return
 ​
-    print(*sol)
+​
+solution()
