883905c5fcae0a784e667d15ffb4823412858fbf
outli2r <80079529+outli2r@users.noreply.github.com>
2023-09-26 13:36:18 +0900

    updated at 2023. 9. 26. 오후 1:36:17

--- baekjoon/1939/solution.py
+++ baekjoon/1939/solution.py
@@ -7,43 +7,46 @@ from collections import deque, defaultdict
 ​
 ​
 def solution():
-    n, m = map(int, input().split())
-    graph = defaultdict(list)
-    max_weight = 0
-    for _ in range(m):
-        a, b, c = map(int, input().split())
-        graph[a].append((b, c))
-        graph[b].append((a, c))
-        max_weight = max(max_weight, c)
-    s, e = map(int, input().split())
+    N, M = map(int, input().split())
+    bridges = defaultdict(list)
+    l, r = 1, 0
 ​
-    def bfs(weight):
-        checked[s] = 1
-        q = deque([s])
+    for _ in range(M):
+        a, b, c = map(int, input().split())
+        bridges[a].append((b, c))
+        bridges[b].append((a, c))
+        r = max(r, c)
+​
+    A, B = map(int, input().split())
+​
+    def bfs(c):
+        q = deque([A])
+        visited = [0] * (N + 1)
+        visited[A] = 1
+​
         while q:
             node = q.popleft()
-            if node == e:
+            if node == B:
                 return True
 ​
-            for next_node, w in graph[node]:
-                if not checked[next_node] and mid <= w:
-                    q.append(next_node)
-                    checked[next_node] = 1
+            for n, w in bridges[node]:
+                if not visited[n] and c <= w:
+                    visited[n] = 1
+                    q.append(n)
 ​
         return False
-    
-    l, r = 0, max_weight
+​
+    answer = 0
     while l <= r:
-        checked = [0] * (n + 1)
-        mid = l + (r - l) // 2
+        mid = (l + r) // 2
         if bfs(mid):
+            answer = max(answer, mid)
             l = mid + 1
         else:
             r = mid - 1
 ​
-    print(r)
+    print(answer)
     return
 ​
 ​
 solution()
-​
