50ebdd739c99d6c4a8efb23efcc9782d02af121b
outli2r <80079529+outli2r@users.noreply.github.com>
2023-09-26 16:36:00 +0900

    updated at 2023. 9. 26. 오후 4:35:59

--- baekjoon/2110/solution.py
+++ baekjoon/2110/solution.py
@@ -8,24 +8,25 @@ input = sys.stdin.readline
 def solution():
     N, C = map(int, input().split())
     house = sorted([int(input()) for _ in range(N)])
-    s, e = 1, house[N - 1]
-    answer = -1
+    l, r = 1, house[-1]
+    answer = 0
 ​
-    while s <= e:
-        mid = (s + e) // 2
+    while l <= r:
+        mid = (l + r) // 2
         cnt = 1
-        last = house[0]
+        prev = house[0]
 ​
         for i in range(1, N):
-            if house[i] - last >= mid:
+            if house[i] - prev >= mid:
                 cnt += 1
-                last = house[i]
+                prev = house[i]
 ​
         if cnt >= C:
             answer = mid
-            s = mid + 1
+            l = mid + 1
+​
         else:
-            e = mid - 1
+            r = mid - 1
 ​
     print(answer)
     return
