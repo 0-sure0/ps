5a98f6cd6729c61e57ec4a388f5085de2cafb9bc
outli2r <80079529+outli2r@users.noreply.github.com>
2023-10-23 13:53:20 +0900

    updated at 2023. 10. 23. 오후 1:53:19

--- baekjoon/14712/solution.py
+++ baekjoon/14712/solution.py
@@ -1,31 +1,31 @@
 // [문제 링크]: https://www.acmicpc.net/problem/14712
 
 import sys
+#sys.stdin = open('test.txt', 'r')
 input = sys.stdin.readline
-def getInts(): return map(int, input().split())
 ​
 ​
-def dfs(k):
-    global cnt
-    if k == N * M:
-        cnt += 1
+N, M = map(int, input().split())
+board = [[0] * (M + 1) for _ in range(N + 1)]
+ans = 0
+​
+def dfs(cnt):
+    global ans
+​
+    if cnt == N * M:
+        ans += 1
         return
-    y, x = k//M + 1, k % M + 1
 ​
-    # 넴모 안놓는 경우
-    dfs(k+1)
+    dfs(cnt + 1)
+    r, c = cnt // M + 1, cnt % M + 1
+    if not board[r - 1][c] or not board[r][c - 1] or not board[r - 1][c - 1]:
+        board[r][c] = 1
+        dfs(cnt + 1)
+        board[r][c] = 0
 ​
-    # 넴모 놓는 경우
-    # 지금 놓은 칸이 2*2 넴모를 구성해 터지지 않는 경우만 놓기
-    if not (board[y-1][x]*board[y][x-1]*board[y-1][x-1]):
-        board[y][x] = 1
-        dfs(k+1)
-        board[y][x] = 0
+    return
 ​
-​
-N, M = getInts()
-board = [[0]*(M+1) for _ in range(N+1)]
-cnt = 0
 dfs(0)
-print(cnt)
+print(ans)
+​
 ​
