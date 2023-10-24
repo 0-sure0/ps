00aa673b0468e553cd5d64d43ab0048d599a8add
outli2r <80079529+outli2r@users.noreply.github.com>
2023-10-24 13:43:16 +0900

    updated at 2023. 10. 24. 오후 1:43:16

--- baekjoon/2580/solution.py
+++ baekjoon/2580/solution.py
@@ -4,45 +4,45 @@ import sys
 #sys.stdin = open('test.txt', 'r')
 input = sys.stdin.readline
 ​
+board = []
+row = [[0] * 10 for _ in range(9)]
+col = [[0] * 10 for _ in range(9)]
+board33 = [[0] * 10 for _ in range(9)]
+zeros = []
 ​
-def solution():
-    board = []
-    board99 = [[0] * 10 for _ in range(10)]
-    row = [[0] * 10 for _ in range(10)]
-    column = [[0] * 10 for _ in range(10)]
-    zeros = []
-    for r in range(9):
-        t = list(map(int, input().split()))
-        for c in range(9):
-            if t[c] == 0:
-                zeros.append((r, c))
-            else:
-                row[r][t[c]] = 1
-                column[c][t[c]] = 1
-                board99[r // 3 * 3 + c // 3][t[c]] = 1
-        board.append(t)
+for r in range(9):
+    t = list(map(int, input().split()))
+    for c in range(9):
+        if t[c] == 0:
+            zeros.append((r, c))
+        else:
+            row[r][t[c]] = 1
+            col[c][t[c]] = 1
+            board33[r // 3 * 3 + c // 3][t[c]] = 1
+    board.append(t)
 ​
-    def dfs(idx):
-        if idx >= len(zeros):
+def dfs(idx):
+    if idx >= len(zeros):
+        return True
+​
+    r, c = zeros[idx]
+    for num in range(1, 10):
+        if row[r][num] or col[c][num] or board33[r // 3 * 3  + c // 3][num]:
+            continue
+​
+        row[r][num] = 1
+        col[c][num] = 1
+        board33[r // 3 * 3 + c // 3][num] = 1
+        board[r][c] = num
+        if dfs(idx + 1):
             return True
+        row[r][num] = 0
+        col[c][num] = 0
+        board33[r // 3 * 3 + c // 3][num] = 0
+        board[r][c] = 0
 ​
-        r, c = zeros[idx]
-        for k in range(1, 10):
-            if board99[r // 3 * 3 + c // 3][k] or row[r][k] or column[c][k]:
-                continue
+    return False
 ​
-            board99[r // 3 * 3 + c // 3][k] = row[r][k] = column[c][k] = 1
-            board[r][c] = k
-            if dfs(idx + 1):
-                return True
-            board99[r // 3 * 3 + c // 3][k] = row[r][k] = column[c][k] = 0
-            board[r][c] = 0
-​
-        return False
-​
-    dfs(0)
-    for r in range(9):
-        print(*board[r])
-    return
-​
-solution()
+dfs(0)
+for r in range(9):
+    print(*board[r])
