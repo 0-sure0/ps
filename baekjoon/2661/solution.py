15a9a338b4ca14ca191c9961557736dec6da94ca
outli2r <80079529+outli2r@users.noreply.github.com>
2023-10-24 20:05:52 +0900

    updated at 2023. 10. 24. 오후 8:05:51

--- baekjoon/2661/solution.py
+++ baekjoon/2661/solution.py
@@ -1,22 +1,41 @@
 // [문제 링크]: https://www.acmicpc.net/problem/2661
 
-def check(num):
-    length = len(num)
-    for idx in range(1, length//2 + 1):
-        if num[-idx:] == num[-(idx*2):-idx]:
-            return False
-    else:
-        return True
-​
-def recursive(num):
-    global N, res
-    if len(num) == N:
-        print(num)
-        exit()
-    for i in '123':
-        if check(num + str(i)):
-            recursive(num + str(i))
-    return
+import sys
+#sys.stdin = open('test.txt', 'r')
+input = sys.stdin.readline
 ​
 N = int(input())
-recursive('1')
+​
+​
+def possible(s):
+    jump = 2
+    while jump < len(s):
+        for i in range(len(s) - jump):
+            prev = s[i:i + jump]
+            for j in range(i + jump, len(s) - jump + 1, jump):
+                if prev == s[j:j+jump]:
+                    return False
+                prev = s[j:j+jump]
+        jump += 1
+    return True
+​
+​
+def dfs(s):
+    if len(s) == N:
+        print(s)
+        return True
+​
+    for i in range(1, 4):
+        if s[-1] == str(i):
+            continue
+​
+        t = s + str(i)
+        if possible(t):
+            if dfs(t):
+                return True
+​
+    return False
+​
+​
+dfs('1')
+​
