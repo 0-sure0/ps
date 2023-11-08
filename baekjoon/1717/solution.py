5b576a84d541f053dbfd1ab79d42ee823e8bd516
outli2r <80079529+outli2r@users.noreply.github.com>
2023-11-08 10:42:59 +0900

    updated at 2023. 11. 8. 오전 10:42:58

--- baekjoon/1717/solution.py
+++ baekjoon/1717/solution.py
@@ -3,37 +3,51 @@
 import sys
 #sys.stdin = open('test.txt', 'r')
 input = sys.stdin.readline
-sys.setrecursionlimit(10**9)
 ​
-def solution():
-    n, m = map(int, input().split())
-    parent = [i for i in range(n + 1)]
 ​
-    def get_parent(node):
-        if node == parent[node]:
-            return node
-        parent[node] = get_parent(parent[node])
-        return parent[node]
+N, M = map(int, input().split())
+parent = [i for i in range(N + 1)]
 ​
-    def union_parent(a, b):
-        a = get_parent(a)
-        b = get_parent(b)
-        if a > b:
-            parent[a] = b
-        else:
-            parent[b] = a
-        return
 ​
-    for _ in range(m):
-        x, a, b = map(int, input().split())
-        if x == 0:
-            union_parent(a, b)
-        else:
-            if get_parent(a) == get_parent(b):
-                print("yes")
-            else:
-                print("no")
+def check_parent(a, b):
+    if a == b:
+        return True
+​
+    cur_a = a
+    while parent[cur_a] != cur_a:
+        cur_a = parent[cur_a]
+​
+    cur_b = b
+    while parent[cur_b] != cur_b:
+        cur_b = parent[cur_b]
+​
+    if cur_a == cur_b:
+        return True
+​
+    return False
+​
+​
+def change_parent(a, b):
+    cur_a = a
+    cur_b = b
+    while parent[cur_a] != cur_a:
+        cur_a = parent[cur_a]
+​
+    while parent[cur_b] != cur_b:
+        cur_b = parent[cur_b]
+​
+    p = min(parent[cur_a], parent[cur_b])
+    parent[cur_a] = p
+    parent[cur_b] = p
     return
 ​
-solution()
 ​
+for _ in range(M):
+    t, a, b = map(int, input().split())
+    if t == 1:
+        if check_parent(a, b):
+            print("yes")
+        else:
+            print("no")
+    else:
+        change_parent(a, b)
