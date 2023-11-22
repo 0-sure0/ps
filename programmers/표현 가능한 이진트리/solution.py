a5df9bfc3a69cd63d1e3ab0d3dcfcac26a9d3315
sure <80079529+0-sure0@users.noreply.github.com>
2023-11-22 23:19:08 +0900

    uploaded at 2023. 11. 22. 오후 11:19:06

---
+++ programmers/표현 가능한 이진트리/solution.py
@@ -0,0 +1,14 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/150367
+
+from math import log2, ceil
+
+def solution(numbers):
+    def dfs(v: int, pow: int) -> int:
+        if pow == 1:
+            return True
+        push_count = 2 ** (pow - 1) - 1
+        left, mid, right = v >> (push_count + 1), (v >> push_count) & 1, v & (2 ** push_count - 1)
+        if not mid and (left or right):
+            return False
+        return min(not left or dfs(left, pow - 1), not right or dfs(right, pow - 1))
+    return [int(dfs(v, ceil(log2(ceil(log2(v + 1)) + 1)))) for v in numbers]
